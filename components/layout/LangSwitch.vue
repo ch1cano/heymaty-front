1dd1991600a3f6a5001de90274799999286a723d
lakeTP <shmelkov15@bk.ru>
2023-11-10 16:38:50 +0300

    fix header and add search

--- components/layout/LangSwitch.vue
+++ components/layout/LangSwitch.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="ls-container">
-    <!-- <a
+	<div class="ls-container">
+		<!-- <a
       href="#"
       v-for="locale in availableLocales"
       :key="locale.code"
@@ -8,40 +8,40 @@
       ><country-flag :country="locale.flag"
     /></a> -->
 
-    <nuxt-link
-      v-for="locale in availableLocales"
-      :key="locale.code"
-      :to="switchLocalePath(locale.code)"
-    >
-      <country-flag :country="locale.flag" />
-    </nuxt-link>
-  </div>
+		<nuxt-link
+			v-for="locale in availableLocales"
+			:key="locale.code"
+			:to="switchLocalePath(locale.code)"
+		>
+			<country-flag :country="locale.flag" />
+		</nuxt-link>
+	</div>
 </template>
 
 <script>
 import CountryFlag from "vue-country-flag";
 export default {
-  name: "LangSwitch",
-  components: { CountryFlag },
-  computed: {
-    availableLocales() {
-      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
-    },
-  },
+	name: "LangSwitch",
+	components: { CountryFlag },
+	computed: {
+		availableLocales() {
+			return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 .ls-container {
-  position: absolute;
-  right: 0px;
-  top: 0px;
-  a {
-    text-decoration: none;
-    &:hover {
-      text-decoration: none;
-      border-bottom: initial;
-    }
-  }
+	position: absolute;
+	right: 0px;
+	top: 0px;
+	a {
+		text-decoration: none;
+		&:hover {
+			text-decoration: none;
+			border-bottom: initial;
+		}
+	}
 }
 </style>
