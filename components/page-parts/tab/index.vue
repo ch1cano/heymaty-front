fbcf4f62e890aad1a59239e17eff54ea5e654dda
andrew.f1 <andrew.f@wellnuts.by>
2023-11-13 03:12:02 +0700

    git add feed

---
+++ components/page-parts/tab/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class=" styled_nav_favorites">
+    <div>
+      <button
+        class="nav_btn"
+        :style="{
+          backgroundColor: currentColor === 'All' ? '#2554F3' : '',
+          color: currentColor === 'All' ? 'white' : '',
+        }"
+        @click="changeColor('All')"
+      >
+        <p>All</p>
+      </button>
+      <button
+        class="nav_btn"
+        :style="{
+          backgroundColor: currentColor === 'Subscribed' ? '#2554F3' : '',
+          color: currentColor === 'Subscribed' ? 'white' : '',
+        }"
+        @click="changeColor('Subscribed')"
+      >
+        <p>Subscribed</p>
+      </button>
+      <button
+        class="nav_btn"
+        :style="{
+          backgroundColor: currentColor === 'For you' ? '#2554F3' : '',
+          color: currentColor === 'For you' ? 'white' : '',
+        }"
+        @click="changeColor('For you')"
+      >
+        <p>For you</p>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentColor: "",
+    };
+  },
+  methods: {
+    changeColor(color) {
+      this.currentColor = color;
+    },
+  },
+};
+</script>
--- pages/Feed/index.vue
+++ pages/Feed/index.vue
@@ -1,45 +1,112 @@
 <template>
-  <div class="tabs">
-    <div class="posts-page">
-      <div class="wrap small flex">
-        <div class="list" v-if="">
-          <div class="spinner"></div>
+  <div>
+    <Loader v-if="publicPostsLoading" />
+
+    <div v-if="!publicPostsLoading">
+      <template>
+        <div class="wrapper">
+          <Tabs />
+          <div class="wrap_ganeral">
+            <div class="content">
+              <div>
+                <div class="favorites_title">
+                  <p>Made for you</p>
+                </div>
+                <div v-if="openPosts.length <= 0" class="entryPosts">
+                  <p>Oh, your matys have no posts yet Keep tuning!</p>
+                </div>
+                <NoMatys v-if="favorites.length <= 0" />
+                <div v-if="openPosts.length > 0" class="post-wrapper">
+                  <Post
+                    v-for="item in openPosts"
+                    :key="item._id"
+                    :images="item.images"
+                    :name="item.userId.name"
+                    :description="item.description"
+                    :createdAt="item.createdAt"
+                    :isPublic="item.isPublic"
+                    :avatar="item.userId.profile.path"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="suggestions_1">
+              <div>
+                <p>Suggestions</p>
+              </div>
+              <div class="card_1"></div>
+              <div class="card_1"></div>
+              <div class="card_1"></div>
+              <div class="card_1"></div>
+            </div>
+          </div>
         </div>
-        <div class="list empty-state" v-if="isFavoritesNoPosts">
-          Oh, your matys have no posts yet. Keep tuning!
-        </div>
-        <div class="list empty-state" v-else-if="isNotHaveFavorites">
-          Oh, you have no matys! Let's start a search together.
-        </div>
-        <div class="list" v-else>
-          <transition name="slide-left" mode="out-in">
-            <posts-list key="publicPosts" isPublic />
-          </transition>
-        </div>
-      </div>
+      </template>
+      {{ data }}
     </div>
   </div>
 </template>
 
 <script>
-import Spinner from "./../../components/ui/Spinner";
-import { mapState, mapActions } from "vuex";
-import PostsList from "@/components/page-parts/posts/PostsList.vue";
+import Loader from "../../components/LoaderFavorites/Loader.vue";
+import NoMatys from "../../pages/NoMatys";
+import Post from "../../components/page-parts/post";
+import { mapState } from "vuex";
+import GirlCard from "@/components/page-parts/index/GirlCard";
+import Tabs from "../../components/page-parts/tab";
 
 export default {
-  name: "PageIndex",
+  name: "PageFeed",
   components: {
-    PostsList,
-    Spinner, // Ensure Spinner is imported correctly
+    Loader,
+    GirlCard,
+    Post,
+    NoMatys,
+    Tabs,
   },
+
   data() {
     return {
+      isLoading: true,
       items: [],
     };
+  },
+
+  async asyncData({ store, route }) {
+    const response = await store.dispatch("favorites/getMyFavorites");
+
+    await store.dispatch("post/resetPosts", null, { root: true });
+
+    const posts = await store.dispatch("post/getAllPublicPosts", null, {
+      root: true,
+    });
+
+    console.log("posts", posts);
+
+    console.log("response", response);
+
+    return response;
+  },
+
+  async mounted() {
+    this.loadData();
+
+    await this.$store.dispatch("post/resetPosts", null, { root: true });
+    await this.$store.dispatch("post/getAllPublicPosts", null, { root: true });
+  },
+  methods: {
+    async asyncData() {
+      await store.dispatch("favorites/getMyFavorites");
+    },
+
+    async loadData() {
+      this.isLoading = false;
+    },
   },
   computed: {
     ...mapState("post", ["pageOpen", "openPosts", "publicPostsLoading"]),
     ...mapState("favorites", ["favorites"]),
+
     isChildShown() {
       return !!this.$route.params.postid;
     },
@@ -57,31 +124,74 @@ export default {
       return !this.publicPostsLoading && !this.openPosts;
     },
   },
-  async mounted() {
-    await this.$store.dispatch("post/resetPosts", null, { root: true });
-    await this.$store.dispatch("post/getAllPublicPosts", null, { root: true });
-  },
 };
 </script>
 
 <style>
-/* Improved styling */
-.tabs {
-  padding: 20px;
-}
-
-.wrap.small.flex {
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-}
-
-.empty-state {
-  margin-bottom: 100px;
-  font-weight: bold;
-  font-size: 42px;
-  line-height: 120%;
+.entryPosts {
+  color: var(--grayscale-black, #161718);
   text-align: center;
-  color: #55555b;
+  font-family: Nunito Sans;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 33px;
+  margin: auto;
+}
+.wrapper {
+  max-width: 1148px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
+
+.wrap_ganeral {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 40px;
+}
+
+.content {
+  max-width: 750px;
+  width: 100%;
+}
+
+.post-wrapper {
+  height: 100vh;
+}
+
+.favorites_title {
+  font-size: 18px;
+  font-weight: 800;
+  margin-bottom: 12px;
+}
+
+.suggestions_1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80dvh;
+}
+
+.suggestions_1 p {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px; /* 133.333% */
+  letter-spacing: -0.5px;
+  width: 343px;
+}
+.card_1 {
+  width: 343px;
+  height: 172px;
+  background-color: #e9edf5;
+  border-radius: 20px;
+  margin-top: 8px;
 }
 </style>
